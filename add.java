80983952095710731d65d4511f7f392c969d69a7
Tejas <jawaletejas1299@gmail.com>
2022-06-12 10:15:37 +0530

    Programs

---
+++ add.java
@@ -0,0 +1,17 @@
+package CODE;
+
+import java.util.Scanner;
+
+public class add {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the two no = ");
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		
+		System.out.println("add = " + (n1+n2));
+	}
+
+}
---
+++ table.java
@@ -0,0 +1,14 @@
+package CODE;
+
+public class table {
+
+	public static void main(String[] args) {
+		int i,n=2;
+		
+		for(i=1;i<=10;i++) {
+			System.out.println(n*i);
+		}
+
+	}
+
+}
